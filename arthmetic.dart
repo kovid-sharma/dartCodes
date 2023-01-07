e413fa6b31b7b7eb1fcbf9dce6f20f1892702500
kovid-sharma <76726175+kovid-sharma@users.noreply.github.com>
2023-01-07 23:50:56 +0530

    finalFor mErge sort

--- arthmetic.dart
+++ arthmetic.dart
@@ -26,7 +26,7 @@ void main() {
   final maxLetters=10;//assuming the name has 5 to 10 letters
   final allPerson= <Person>[];
   //print('FirstName                LastName                    Date of Birth');
-  for (int i=1;i<=1e4;i++) {
+  for (int i=1;i<=1e5;i++) {
     final arr = 'abcdefghijklmnopqrstuvwxyz';  //arr is the array where all the letters are accessed this list by generating random index
     final first = randSt(rand,minLetters,maxLetters,arr);
     final last = randSt(rand,minLetters,maxLetters,arr);
@@ -48,6 +48,56 @@ void main() {
   timer.stop();
   print('Time taken: ${timer.elapsedMilliseconds} milliseconds');
 }
+void mergeSort(List<Person> arr, int l, int r) {
+  if (l < r) {
+    final m = (l + r) ~/ 2;
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+  }
+}
+void merge(List<Person> arr, int l, int m, int r) {
+  final n1 = m - l + 1;
+  final n2 = r - m;
+
+  final L = <Person>[];
+  final R = <Person>[];
+
+
+  for (int i = 0; i < n1; i++) {
+    L.add(arr[l + i]);
+  }
+  for (int j = 0; j < n2; j++) {
+    R.add(arr[m + 1 + j]);
+  }
+
+  int i = 0;
+  int j = 0;
+  int k = l;
+  while (i < n1 && j < n2) {
+    if (L[i].dob.isBefore(R[j].dob)) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+}
+
 void bubbleSort(List<Person> persons) {
   final n = persons.length;
   for (int i = 0; i < n - 1; i++) {
